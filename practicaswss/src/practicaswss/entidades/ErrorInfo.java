f48501f4a5f08c361595f923979ebd4b24c7789a
feynman <codosero@hotmail.com>
2018-07-04 19:44:51 +0200

    Código Rest del día

---
+++ practicaswss/src/practicaswss/entidades/ErrorInfo.java
@@ -0,0 +1,44 @@
+package practicaswss.entidades;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class ErrorInfo {
+
+	/**
+	 * Respuesta errores
+	 */
+
+	private int codigo;
+	private String mensaje;
+	
+	public ErrorInfo(){
+		
+	}
+
+	public ErrorInfo(int codigo, String mensaje) {
+		super();
+		this.codigo = codigo;
+		this.mensaje = mensaje;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getMensaje() {
+		return mensaje;
+	}
+
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
+
+}
--- practicaswss/src/practicaswss/rest/servicios/GestionClientes.java
+++ practicaswss/src/practicaswss/rest/servicios/GestionClientes.java
@@ -5,19 +5,28 @@ import java.util.Collection;
 import java.util.HashMap;
 
 import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.HttpMethod;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
 import practicaswss.entidades.Cliente;
+import practicaswss.entidades.ErrorInfo;
 
 // La llamada sería
 // /serviciosrest/clientes/{nif}
 //Para probar http://localhost:8080/practicaswss/v1.0/clientes/51
+//@SessionScoped   //crea la cookie
 
 @Path("/clientes")
+@ApplicationScoped // funciona para todos los clientes
 public class GestionClientes {
 	
 	private 
@@ -39,7 +48,7 @@ public class GestionClientes {
 	
 	@GET
 	@Path("/{nif}") // /{nif:[0-9]{8}[A-Z] con esto validamos 8 numeros 1 letra
-	@Produces({"application/json","application/xml"}) //el xml lo podemos obviar
+	@Produces({"application/xml","application/json"}) //el xml lo podemos obviar
 	public Response buscarPorNif(
 			@PathParam(value = "nif")
 			String nifCliente
@@ -68,11 +77,49 @@ public class GestionClientes {
 		
 	}
 	
+	//para invocarlo por RestFull
+	@PUT
+	@POST
+	@Path("/")
+	@Consumes({"application/xml","application/json"})
+	public Response altaCliente(Cliente cte) {
+		
+		if(tablaClientes.containsKey(cte.getNif())) {
+			return Response.status(HttpURLConnection.HTTP_BAD_REQUEST)
+					.entity(
+						new ErrorInfo(-2120, "Existe cliente con el NIF:"+cte.getNif())	
+							)
+					//.type() se pueden poner cabeceras para obligar que nos las pasen
+					.build(); //400 return
+					
+		}
+		else {
+			tablaClientes.put(cte.getNif(), cte);
+			return Response.ok().build();
+		}
+	}
+	
+	@POST
+	@Path("/{nif:[0-9]{8}[A-Z]}")
+	@Consumes({"application/xml","application/json"})
+	public Response modificarCliente(Cliente cte) {
+		
+		tablaClientes.put(cte.getNif(), cte);
+		
+		return Response.ok().build();
+	}
 	
 	
+	@DELETE
+	@Path("/{nif:[0-9]{8}[A-Z]}")
+	public Response borrarCliente(
+			@PathParam("nif")
+			String nif) {
+		
+		tablaClientes.remove(nif);
+		
+		return Response.ok().build();
+	}
 	
 	
-	
-	
-
 }
